51ea5c588daeff1d29c45d025f842a4f7a9c0636
曹鹏 <caopeng@caopengdemacbook-pro.local>
2018-01-20 20:31:41 +0800

    Initial Commit

---
+++ FakeWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FakeWeibo
+//
+//  Created by 曹鹏 on 2018/1/20.
+//  Copyright © 2018年 曹鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
